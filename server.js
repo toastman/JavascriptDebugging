ee9fb128726140fa591decef04618d4796ba5606
Andrii Vandakurov <andriy.vandakurov@gmail.com>
2015-10-28 01:28:20 +0200

    Init commit

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express'),
+    request = require('request'),
+    app = express(),
+    appPort = process.env.PORT || 3000;
+
+app.use('/data', function (req, res) {
+    var url = 'http://private-aa10d-global1.apiary-mock.com/poll/1';
+    req.pipe(request(url)).pipe(res);
+});
+
+app.listen(appPort, function () {
+    console.log('Server is up and running on %d port', appPort);
+});
