1c5f67d91cebc6a3f8b84547512aa898706e0133
Andrii Vandakurov <andriy.vandakurov@gmail.com>
2015-10-28 10:21:25 +0200

    Update

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 var express = require('express'),
     request = require('request'),
     app = express(),
-    appPort = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000;
+    server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.use('/data', function (req, res) {
     var url = 'http://private-aa10d-global1.apiary-mock.com/poll/1';
     req.pipe(request(url)).pipe(res);
 });
 
-app.listen(appPort, function () {
-    console.log('Server is up and running on %d port', appPort);
+var server = app.listen(server_port, server_ip_address, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
 });
